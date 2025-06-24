3088f8ffdd0d83b50fda591a958c45e67450ec25
Mu1hammed <m.elnshar@pksaudi.com>
2025-06-24 20:12:53 +0300

    fix

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return Auth::check() ? to_route('home') : to_route('login');
 });
 
 Auth::routes();
